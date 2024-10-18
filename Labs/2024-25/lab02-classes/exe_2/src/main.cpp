21ad78bc061411a8126a0aabb0a32a868960f840
Federico Gatti <federico@MacBook-Air-di-IMATI-5.local>
2024-10-18 16:05:33 +0200

    added the solution

--- Labs/2024-25/lab02-classes/exe_2/src/main.cpp
+++ Labs/2024-25/lab02-classes/exe_2/src/main.cpp
@@ -3,7 +3,7 @@
 #include <vector>
 #include "utils.h"
 
-static const double step_size = 1e-4;
+static const double step_size = 1e-6;
 static type_fun func = [](double const& x){return x*x-2.;};
 static type_fun der_func = [](double const& x){return 2.*x;};
 
@@ -17,7 +17,7 @@ int main(int argc, char** argv)
     double rtol = 1e-5;
     double stol = 1e-5;
     
-    root_method.push_back(make_shared<Newton>(2.0, func, max_iter, rtol, stol, nullopt, 1e-6)); // Secant method 
+    root_method.push_back(make_shared<Newton>(2.0, func, max_iter, rtol, stol, nullopt, step_size)); // Secant method 
     root_method.push_back(make_shared<Newton>(2.0, func, max_iter, rtol, stol, der_func)); // Pure Newton method
     root_method.push_back(make_shared<Bisection>(0, 2, func, max_iter, rtol, stol)); // Bisection 
 

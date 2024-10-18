0b45e377eb5e3efc618588b834a54a8de88fcab0
Federico Gatti <federico@MacBook-Air-di-IMATI-5.local>
2024-10-18 09:12:25 +0200

    added memory folder

--- Labs/2024-25/lab02-classes/memory/memory.cpp
+++ Labs/2024-25/lab02-classes/memory/memory.cpp
@@ -13,6 +13,8 @@ size_t Counter::count = 0;
 
 void a_function() {
   int z;
+  std::vector<int> v(3);
+  std::cout << "Heap memory 2 (v after init): " << v.data() << std::endl;
   std::cout << "Stack memory (z): " << &z << std::endl;
 }
 
@@ -31,7 +33,7 @@ int main() {
     auto pt = std::make_shared<Counter>();
     std::cout << "Heap memory (v after init): " << v.data() << std::endl;
     std::cout << "Heap memory (pt): " << pt.get() << std::endl;
-    for(int i = 0; i < 1000000; i++)
+    for(int i = 0; i < 1e5; i++)
       v.push_back(0);
     std::cout << "Heap memory (v after push_back): " << v.data() << std::endl;
     

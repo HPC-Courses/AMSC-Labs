2ad6d4d3c1a8697f1c50a5bfd92553392bc280e2
Federico Gatti <federico@MacBook-Air-di-IMATI-5.local>
2024-11-22 10:08:57 +0100

    template naive implementation

--- Labs/2024-25/twoexercises/05-stl-templates/02-derivatives/Derivatives.hpp
+++ Labs/2024-25/twoexercises/05-stl-templates/02-derivatives/Derivatives.hpp
@@ -1,6 +1,27 @@
 #ifndef DERIVATIVES_HPP
 #define DERIVATIVES_HPP
 
+#include <functional>
+#include <type_traits>
 
+using func_t = std::function<double(double)>;
+
+
+class NaiveNthDerivative {
+public:
+  NaiveNthDerivative(const func_t &f, double h, unsigned int deg) : 
+    m_f(f), 
+    m_h(h), 
+    m_deg(deg) {}
+  double operator()(double x) {
+  }
+private:
+  double f_prime(double x, unsigned deg) {
+  }
+
+  const func_t m_f;
+  const double m_h;
+  const unsigned int m_deg;
+};
 
 #endif /* DERIVATIVES_HPP */
